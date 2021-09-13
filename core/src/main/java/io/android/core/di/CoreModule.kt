6f2dac1a18c1afbe457e914acfe7abe375cd8797
petersam <man.sanji23@gmail.com>
2021-09-13 17:16:06 +0700

    certs pinning

--- core/src/main/java/io/android/core/di/CoreModule.kt
+++ core/src/main/java/io/android/core/di/CoreModule.kt
@@ -11,6 +11,7 @@ import io.android.core.domain.repository.GameRepository
 import java.util.concurrent.TimeUnit
 import net.sqlcipher.database.SQLiteDatabase
 import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -43,9 +44,16 @@ val networkModule = module {
 }
 
 fun provideOkHttpClient(): OkHttpClient {
+    val hostname = "www.amiiboapi.com"
+    val certificatePinner = CertificatePinner.Builder()
+        .add(hostname, "sha256/njwbWjzkl4OkPq2NlQXQBFOu9bzJu9Vp4Xxy1rG2jWU=")
+        .add(hostname, "sha256/jQJTbIh0grw0/1TkHSumWb+Fs0Ggogr621gT3PvPKG0=")
+        .add(hostname, "sha256/C5+lpZ7tcVwmwQIMcRtPbsQtWLABXhQzejna0wHFr8M=")
+        .build()
     val client = OkHttpClient.Builder()
         .connectTimeout(30, TimeUnit.SECONDS)
         .readTimeout(30, TimeUnit.SECONDS)
+        .certificatePinner(certificatePinner)
     if (BuildConfig.DEBUG) {
         client.addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
     }
